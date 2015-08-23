1eeae4ddf9ecc7d4d9cbdd6b91b76abbaff87a1e
cat8dog <catherine.reyto@gmail.com>
2015-08-23 15:29:14 -0400

    Objective C file connects to TableView but Done and Cancel buttons can't get wired up

---
+++ Lister-stage-05/Lister-stage-05/AddToDoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddToDoViewController.swift
+//  Lister-stage-05
+//
+//  Created by Catherine Reyto on 2015-08-23.
+//  Copyright (c) 2015 Catherine Reyto. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Lister-stage-05/Lister-stage-05/MyViewController.h
@@ -0,0 +1,10 @@
+
+#ifndef _MyViewController_h
+#define _MyViewController_h
+
+
+#endif
+
+@interface MyViewController(Workaround) -(IBAction)unwindToMyViewController: (UIStoryboardSegue *)segue;
+
+@end
